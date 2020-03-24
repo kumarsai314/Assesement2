f6b90de376ae594d7bd9721135315d8cf503c718
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-03-23 22:13:50 -0700

    Update removeoccurance.py

--- removeoccurance.py
+++ removeoccurance.py
@@ -1,5 +1,5 @@
 list1 = [5, 20, 15, 20, 25, 50, 20]
-for x in list1:
-    if x==20 :
-     list1.remove(20)
+for x in list1:  
+    if x==20 :     
+     list1.remove(20) # if it finds the element 20 in list it removes 20. using remove method in list.
 print(list1)

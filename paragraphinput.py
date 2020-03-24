a67ce3b98b11c682e9ab2a6f1929785b1cd40524
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-03-23 22:15:43 -0700

    Update paragraphinput.py

--- paragraphinput.py
+++ paragraphinput.py
@@ -1,8 +1,8 @@
 x=input("Take paragraph as a input :")
 print(x)
-y=x.split()
+y=x.split() # using split method it divides each word in a list  with coma seperated.
 print(y)
-z=set(y)
+z=set(y) # converting list to set. Set only accepts Unique elements. it won't accept duplicates.
 print(z)
 
     

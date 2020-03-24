b56c3fcdae5360d9245fe426bf878697a8fb70ec
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-03-23 22:12:31 -0700

    Update tupleinputandremove.py

--- tupleinputandremove.py
+++ tupleinputandremove.py
@@ -1,12 +1,12 @@
 s= input("Enter some numbers with seperated")
-x=tuple(s)
+x=tuple(s) # converting input to Tuple
 type(x)
 print(x)
-y=list(x)
+y=list(x) # converting tuple to List
 print(y)
-y.pop(0)
+y.pop(0) # remvoing first element using pop method in list
 print(y)
-y.pop()
+y.pop() # removing last element using pop method in list. If we don't specify the position of the element it defaultly deletes last element.
 print(y)
-x=tuple(y)
+x=tuple(y) # converting list to Tuple
 print(x)

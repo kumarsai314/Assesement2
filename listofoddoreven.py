42efa134a0ff0d51301610e6c811affc148d144b
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-03-23 22:18:57 -0700

    Update listofoddoreven.py

--- listofoddoreven.py
+++ listofoddoreven.py
@@ -1,11 +1,11 @@
 s= input("Enter the list of number:")
 x= list(s)
-even=[]
-odd=[]
+even=[] #  defining empty list
+odd=[] # defining empty list 
 for i in x :
     if i%2==0:
-        even.append(i)
+        even.append(i) # if the condition is true then using append method number adds to  even  list everytime.
     else:
-         odd.append(i)
+         odd.append(i)  # if the condition is false then using append method number adds to  odd  list everytime.
 print(odd)
 print(even)
